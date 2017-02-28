984a86bf87ed8ceb8433292da3c692dfea09b9f3
soap94 <hjo@cantops.biz>
2017-02-28 16:28:55 +0900

    first upload
    first time uploading

---
+++ CODE/frmCODE.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CODE
+{
+    partial class frmCODE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bgWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.MainTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // bgWorker1
+            // 
+            this.bgWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorker1_RunWorkerCompleted);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Gulim", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 57);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(436, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainTimer
+            // 
+            this.MainTimer.Enabled = true;
+            this.MainTimer.Tick += new System.EventHandler(this.MainTimer_Tick);
+            // 
+            // frmCODE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 173);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCODE";
+            this.Text = "CODE v2017.02.02";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmCODE_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer MainTimer;
+    }
+}
+
---
+++ CODE/frmCODE.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+//using System.Diagnostics;
+//using System.Runtime.InteropServices;
+//using System.IO;
+//using System.Collections;
+
+namespace CODE
+{
+    public partial class frmCODE : Form
+    {
+        private const int MAX_TASK = 3;
+        private BackgroundWorker bgWorker1;
+        private bool[] abMyThreadStop = new bool[MAX_TASK];
+        private Thread[] aMyThread = new Thread[MAX_TASK];
+        private ulong g_uGlobalTickCnt = 0;
+        private uint g_uTryCnt0 = 0;
+        private uint g_uTryCnt1 = 0;
+
+        //private Stopwatch watch = new Stopwatch();
+
+        private void bgWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            try
+            {
+                //this.Text = "Using Task Parallel & Background Worker";
+                this.Text = g_uGlobalTickCnt.ToString();
+
+                if (g_uGlobalTickCnt % 2 == 0)
+                    this.button1.BackColor = Color.Red;
+                else
+                    this.button1.BackColor = Color.Blue;
+
+                this.label1.Text = g_uGlobalTickCnt.ToString();
+                this.button1.Text = string.Format("{0} vs {1}", g_uTryCnt0, g_uTryCnt1);
+            }
+            finally
+            {
+            }
+        }
+
+        private void ThMyThread0()
+        {
+            long subb = 0;
+            while (abMyThreadStop[0] == false)
+            {
+                for (int i = 0; i < 10000; i++)
+                    for (int j = 0; j < 10000; j++)
+                        subb = i * j;
+
+                Thread.Sleep(100);
+                g_uTryCnt0++;
+            }
+        }
+
+        private void ThMyThread1()
+        {
+            long subb = 0;
+            while (abMyThreadStop[1] == false)
+            {
+                for (int i = 0; i < 1000; i++)
+                    for (int j = 0; j < 1000; j++ )
+                        subb = i*j;
+
+                Thread.Sleep(100);
+                g_uTryCnt1++;
+            }
+        }
+
+        private void ThMyThread2()
+        {
+            while (abMyThreadStop[2] == false)
+            {
+                g_uGlobalTickCnt++;
+                Thread.Sleep(100);
+            }
+        }
+
+        private void MyTask0()
+        {
+            this.aMyThread[0] = new Thread(new ThreadStart(this.ThMyThread0));
+            abMyThreadStop[0] = false;
+            this.aMyThread[0].Start();
+        }
+
+        private void MyTask1()
+        {
+            this.aMyThread[1] = new Thread(new ThreadStart(this.ThMyThread1));
+            abMyThreadStop[1] = false;
+            this.aMyThread[1].Start();
+        }
+
+        private void MyTask2()
+        {
+            MainTimer.Enabled = true;
+
+            this.aMyThread[2] = new Thread(new ThreadStart(this.ThMyThread2));
+            abMyThreadStop[2] = false;
+            this.aMyThread[2].Start();            
+        }
+
+        public frmCODE()
+        {
+            InitializeComponent();
+
+            // Task Parallel Method
+            Parallel.For(0, MAX_TASK, i =>
+            {
+                var idx = i;
+
+                switch (idx)
+                {
+                    case 0:
+                        Task.Factory.StartNew(new Action(MyTask0));
+                        break;
+                    case 1:
+                        Task.Factory.StartNew(new Action(MyTask1));
+                        break;
+                    case 2:
+                        Task.Factory.StartNew(new Action(MyTask2));
+                        break;
+                }
+            }); // Parallel.For
+        }
+ 
+        private void frmCODE_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //watch.Reset();
+            MainTimer.Enabled = false;
+
+            bgWorker1.Dispose();
+            for (int i = 0; i < MAX_TASK; i++)
+            {
+                abMyThreadStop[i] = true;
+            }
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            this.bgWorker1.RunWorkerAsync();
+        }
+    }
+}
